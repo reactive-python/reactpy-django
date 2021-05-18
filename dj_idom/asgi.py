5a03f824fe365ca359397d2fcd0c9d8cfee02788
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-05-17 22:33:32 -0700

    barebones websocket consumer

--- dj_idom/asgi.py
+++ dj_idom/asgi.py
@@ -9,8 +9,27 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_idom.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj_idom.settings")
 
-application = get_asgi_application()
+# Fetch ASGI application before importing dependencies that require ORM models.
+http_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from .consumers import CommandConsumer
+
+application = ProtocolTypeRouter(
+    {
+        # ASGI app has concurrency problems, see
+        # See https://github.com/django/channels/issues/1587
+        "http": http_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter([url("", CommandConsumer().as_asgi())]))
+        ),
+    }
+)
---
+++ dj_idom/consumers.py
@@ -0,0 +1,18 @@
+"""Anything used to construct a websocket endpoint"""
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+
+class CommandConsumer(AsyncJsonWebsocketConsumer):
+    """Websocket communication."""
+
+    # INITIAL CONNECTION
+    async def connect(self):
+        """When the browser attempts to connect to the server."""
+        # Accept the connection
+        await self.accept()
+        pass
+
+    # RECEIVING COMMANDS
+    async def receive_json(self, content, **kwargs):
+        """When the browser attempts to send a message to the server."""
+        pass
