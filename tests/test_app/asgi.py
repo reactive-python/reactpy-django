c31f4a0120fe94c08bf044f423627b057df8b989
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-06-20 14:21:41 -0700

    fix flake8 errors

--- tests/test_app/asgi.py
+++ tests/test_app/asgi.py
@@ -12,16 +12,18 @@ import os
 from django.conf.urls import url
 from django.core.asgi import get_asgi_application
 
+from django_idom import IdomAsyncWebSocketConsumer  # noqa: E402
+
 from .views import HelloWorld
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_app.settings")
 
 # Fetch ASGI application before importing dependencies that require ORM models.
 http_asgi_app = get_asgi_application()
 
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
 
-from django_idom import IdomAsyncWebSocketConsumer
 
 application = ProtocolTypeRouter(
     {
--- tests/test_app/tests.py
+++ tests/test_app/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
 
 # Create your tests here.
+class Temp(TestCase):
+    pass
