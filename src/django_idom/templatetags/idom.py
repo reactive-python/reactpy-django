8b0b363b1deb7641b25b52d309688c27d3f14106
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-07-22 01:09:28 -0700

    styling fix
    attempt re-adding tag stuff
    
    styling fix

--- src/django_idom/templatetags/idom.py
+++ src/django_idom/templatetags/idom.py
@@ -1,13 +1,14 @@
 from django import template
-from django.urls import reverse
 
 
 register = template.Library()
+
 
 # Template tag that renders the IDOM scripts
 @register.inclusion_tag("idom/head_content.html")
 def idom_scripts():
     pass
+
 
 # Template tag that renders an empty idom root object
 @register.inclusion_tag("idom/root.html")
