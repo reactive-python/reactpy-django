f72b2d6430f45234e09844258001bc31ca324d78
rmorshea <ryan.morshead@gmail.com>
2021-08-11 18:33:22 -0700

    remove unused config option

--- src/django_idom/config.py
+++ src/django_idom/config.py
@@ -5,7 +5,6 @@ from idom.core.proto import ComponentConstructor
 
 
 IDOM_REGISTERED_COMPONENTS: Dict[str, ComponentConstructor] = {}
-IDOM_IGNORE_INSTALLED_APPS = set(getattr(settings, "IDOM_IGNORE_INSTALLED_APPS", []))
 
 IDOM_BASE_URL = getattr(settings, "IDOM_BASE_URL", "_idom/")
 IDOM_WEBSOCKET_URL = IDOM_BASE_URL + "websocket/"
