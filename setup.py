27812fc0be845c5523924d640c50b0562ed57153
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-09-09 21:34:49 -0700

    manually install peer dependencies

--- setup.py
+++ setup.py
@@ -119,6 +119,13 @@ def build_javascript_first(cls):
                 npm = shutil.which("npm")  # this is required on windows
                 if npm is None:
                     raise RuntimeError("NPM is not installed.")
+
+                # Required on when using NPM >3
+                log.info(f"> Installing rollup, react, and react-dom")
+                subprocess.run(f"{npm} install rollup".split(), cwd=js_dir, check=True)
+                subprocess.run(f"{npm} install react".split(), cwd=js_dir, check=True)
+                subprocess.run(f"{npm} install react-dom".split(), cwd=js_dir, check=True)
+
                 for args in (f"{npm} install", f"{npm} run build"):
                     args_list = args.split()
                     log.info(f"> {list2cmdline(args_list)}")
