7648256e9a93d8ff0950d18f999cd6420e1bc6c5
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-09-27 02:49:21 -0700

    IDOM_WS_RECONNECT -> IDOM_WS_RECONNECT_TIMEOUT

--- src/django_idom/templatetags/idom.py
+++ src/django_idom/templatetags/idom.py
@@ -9,7 +9,7 @@ from django_idom.config import (
     IDOM_REGISTERED_COMPONENTS,
     IDOM_WEB_MODULES_URL,
     IDOM_WEBSOCKET_URL,
-    IDOM_WS_RECONNECT,
+    IDOM_WS_RECONNECT_TIMEOUT,
 )
 
 
@@ -27,7 +27,7 @@ def idom_component(_component_id_, **kwargs):
         "class": class_,
         "idom_websocket_url": IDOM_WEBSOCKET_URL,
         "idom_web_modules_url": IDOM_WEB_MODULES_URL,
-        "idom_ws_reconnect": IDOM_WS_RECONNECT,
+        "idom_ws_reconnect_timeout": IDOM_WS_RECONNECT_TIMEOUT,
         "idom_mount_uuid": uuid4().hex,
         "idom_component_id": _component_id_,
         "idom_component_params": urlencode({"kwargs": json_kwargs}),
