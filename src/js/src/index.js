b36ac3773090712ef719d08ebe867a35bafc7bf6
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-09-27 02:37:30 -0700

    change tab width to 2

--- src/js/src/index.js
+++ src/js/src/index.js
@@ -4,34 +4,34 @@ import { mountLayoutWithWebSocket } from "idom-client-react";
 let LOCATION = window.location;
 let WS_PROTOCOL = "";
 if (LOCATION.protocol == "https:") {
-	WS_PROTOCOL = "wss://";
+  WS_PROTOCOL = "wss://";
 } else {
-	WS_PROTOCOL = "ws://";
+  WS_PROTOCOL = "ws://";
 }
 let WS_ENDPOINT_URL = WS_PROTOCOL + LOCATION.host + "/";
 
 export function mountViewToElement(
-	mountPoint,
-	idomWebsocketUrl,
-	idomWebModulesUrl,
-	maxReconnectTimeout,
-	viewId,
-	queryParams
+  mountPoint,
+  idomWebsocketUrl,
+  idomWebModulesUrl,
+  maxReconnectTimeout,
+  viewId,
+  queryParams
 ) {
-	const fullWebsocketUrl =
-		WS_ENDPOINT_URL + idomWebsocketUrl + viewId + "/?" + queryParams;
+  const fullWebsocketUrl =
+    WS_ENDPOINT_URL + idomWebsocketUrl + viewId + "/?" + queryParams;
 
-	const fullWebModulesUrl = LOCATION.origin + "/" + idomWebModulesUrl;
-	const loadImportSource = (source, sourceType) => {
-		return import(
-			sourceType == "NAME" ? `${fullWebModulesUrl}${source}` : source
-		);
-	};
+  const fullWebModulesUrl = LOCATION.origin + "/" + idomWebModulesUrl;
+  const loadImportSource = (source, sourceType) => {
+    return import(
+      sourceType == "NAME" ? `${fullWebModulesUrl}${source}` : source
+    );
+  };
 
-	mountLayoutWithWebSocket(
-		mountPoint,
-		fullWebsocketUrl,
-		loadImportSource,
-		maxReconnectTimeout
-	);
+  mountLayoutWithWebSocket(
+    mountPoint,
+    fullWebsocketUrl,
+    loadImportSource,
+    maxReconnectTimeout
+  );
 }
