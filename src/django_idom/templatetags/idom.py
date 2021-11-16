737cbf29f385470dfe138141663738a6517fb70f
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-11-16 06:04:39 -0800

    allow for loader html in template tag

--- src/django_idom/templatetags/idom.py
+++ src/django_idom/templatetags/idom.py
@@ -20,10 +20,12 @@ def idom_component(_component_id_, **kwargs):
     _register_component(_component_id_)
 
     class_ = kwargs.pop("class", "")
+    loader = kwargs.pop("loader", "")
     json_kwargs = json.dumps(kwargs, separators=(",", ":"))
 
     return {
         "class": class_,
+        "loader": loader,
         "idom_websocket_url": IDOM_WEBSOCKET_URL,
         "idom_web_modules_url": IDOM_WEB_MODULES_URL,
         "idom_ws_max_reconnect_delay": IDOM_WS_MAX_RECONNECT_DELAY,
