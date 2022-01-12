8f57a9c11288e6c88700aa993a8778897d193309
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-01-12 04:16:55 -0800

    clean up caching schema

--- src/django_idom/config.py
+++ src/django_idom/config.py
@@ -12,17 +12,7 @@ IDOM_WEBSOCKET_URL = IDOM_BASE_URL + "websocket/"
 IDOM_WEB_MODULES_URL = IDOM_BASE_URL + "web_module/"
 IDOM_WS_MAX_RECONNECT_DELAY = getattr(settings, "IDOM_WS_MAX_RECONNECT_DELAY", 604800)
 
-_CACHES = getattr(settings, "CACHES", {})
-if _CACHES:
-    if "idom_web_modules" in getattr(settings, "CACHES", {}):
-        IDOM_WEB_MODULE_CACHE = "idom_web_modules"
-    else:
-        IDOM_WEB_MODULE_CACHE = DEFAULT_CACHE_ALIAS
+if "idom" in getattr(settings, "CACHES", {}):
+    IDOM_CACHE = "idom"
 else:
-    IDOM_WEB_MODULE_CACHE = None
-
-
-# the LRU cache size for the route serving IDOM_WEB_MODULES_DIR files
-IDOM_WEB_MODULE_LRU_CACHE_SIZE = getattr(
-    settings, "IDOM_WEB_MODULE_LRU_CACHE_SIZE", None
-)
+    IDOM_CACHE = DEFAULT_CACHE_ALIAS
