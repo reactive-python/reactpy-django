194aba440d207c6903db92c3c3af8bf2b53daa7e
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-01-13 19:43:32 -0800

    set IDOM_CACHE to the cache backend object

--- src/django_idom/config.py
+++ src/django_idom/config.py
@@ -1,7 +1,7 @@
 from typing import Dict
 
 from django.conf import settings
-from django.core.cache import DEFAULT_CACHE_ALIAS
+from django.core.cache import DEFAULT_CACHE_ALIAS, caches
 from idom.core.proto import ComponentConstructor
 
 
@@ -13,6 +13,6 @@ IDOM_WEB_MODULES_URL = IDOM_BASE_URL + "web_module/"
 IDOM_WS_MAX_RECONNECT_DELAY = getattr(settings, "IDOM_WS_MAX_RECONNECT_DELAY", 604800)
 
 if "idom" in getattr(settings, "CACHES", {}):
-    IDOM_CACHE = "idom"
+    IDOM_CACHE = caches["idom"]
 else:
-    IDOM_CACHE = DEFAULT_CACHE_ALIAS
+    IDOM_CACHE = caches[DEFAULT_CACHE_ALIAS]
--- src/django_idom/views.py
+++ src/django_idom/views.py
@@ -8,20 +8,17 @@ from idom.config import IDOM_WED_MODULES_DIR
 from .config import IDOM_CACHE
 
 
-idom_cache = caches[IDOM_CACHE]
-
-
 async def web_modules_file(request: HttpRequest, file: str) -> HttpResponse:
     """Gets JavaScript required for IDOM modules at runtime. These modules are
     returned from cache if available."""
     path = IDOM_WED_MODULES_DIR.current.joinpath(*file.split("/")).absolute()
     last_modified_time = os.stat(path).st_mtime
     cache_key = f"django_idom:web_module:{path}"
-    response = await idom_cache.aget(cache_key, version=last_modified_time)
+    response = await IDOM_CACHE.aget(cache_key, version=last_modified_time)
     if response is None:
         async with async_open(path, "r") as fp:
             response = HttpResponse(await fp.read(), content_type="text/javascript")
-        await idom_cache.aset(
+        await IDOM_CACHE.aset(
             cache_key, response, timeout=None, version=last_modified_time
         )
     return response
