157ecc4ae1cbed5667663a00b337d75f7199b9ed
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-01-20 20:30:53 -0800

    v0.0.2

--- src/django_idom/__init__.py
+++ src/django_idom/__init__.py
@@ -2,5 +2,5 @@ from .websocket.consumer import IdomWebsocket
 from .websocket.paths import IDOM_WEBSOCKET_PATH
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __all__ = ["IDOM_WEBSOCKET_PATH", "IdomWebsocket"]
