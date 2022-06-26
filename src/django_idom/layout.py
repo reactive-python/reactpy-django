8b51988bf337ed77502bf587d09c3a9d8f31c6ce
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-06-25 22:12:20 -0700

    Reset async unsafe flag to the previous value

--- src/django_idom/layout.py
+++ src/django_idom/layout.py
@@ -12,7 +12,16 @@ class DjangoLayout(Layout):
     def _create_layout_update(self, old_state) -> LayoutUpdate:
         """Create a layout update, but set ALLOW ASYNC UNSAFE flags prior.
         This allows the Django ORM to be used within components."""
+        async_unsafe_prev = os.environ.get("IDOM_ALLOW_ASYNC_UNSAFE", None)
+
+        # Set ALLOW ASYNC UNSAFE to True
         os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
         layout_update = super()._create_layout_update(old_state)
-        os.environ.pop("DJANGO_ALLOW_ASYNC_UNSAFE")
+
+        # Reset async unsafe flag to the previous value
+        if async_unsafe_prev is not None:
+            os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = async_unsafe_prev
+        else:
+            os.environ.pop("DJANGO_ALLOW_ASYNC_UNSAFE")
+
         return layout_update
