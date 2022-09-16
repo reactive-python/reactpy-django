83527d21939867763740f110580fd6a64ea1ea8c
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-09-16 03:21:13 -0700

    formatting

--- src/django_idom/utils.py
+++ src/django_idom/utils.py
@@ -6,7 +6,7 @@ import os
 import re
 from fnmatch import fnmatch
 from importlib import import_module
-from typing import Any, Callable
+from typing import Callable
 
 from django.template import engines
 from django.utils.encoding import smart_str
