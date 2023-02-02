f5cc22910c35c319ddf10841a4364d055c4e08dd
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-02-01 17:11:21 -0800

    log when idom database is not ready at startup

--- src/django_idom/apps.py
+++ src/django_idom/apps.py
@@ -1,9 +1,12 @@
-import contextlib
+import logging
 
 from django.apps import AppConfig
 from django.db.utils import OperationalError
 
 from django_idom.utils import ComponentPreloader, db_cleanup
+
+
+_logger = logging.getLogger(__name__)
 
 
 class DjangoIdomConfig(AppConfig):
@@ -16,6 +19,8 @@ class DjangoIdomConfig(AppConfig):
         # Delete expired database entries
         # Suppress exceptions to avoid issues with `manage.py` commands such as
         # `test`, `migrate`, `makemigrations`, or any custom user created commands
-        # where the database may not be preconfigured.
-        with contextlib.suppress(OperationalError):
+        # where the database may not be ready.
+        try:
             db_cleanup(immediate=True)
+        except OperationalError:
+            _logger.debug("IDOM database was not ready at startup. Skipping cleanup...")
