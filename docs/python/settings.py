2c8d677ad5e4b8531281d5548e3567fc82497fe7
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-02-21 15:23:01 -0800

    minor comment cleanup

--- docs/python/settings.py
+++ docs/python/settings.py
@@ -1,7 +1,7 @@
-# IDOM works best with a multiprocessing-safe and thread-safe cache backend.
+# IDOM requires a multiprocessing-safe and thread-safe cache.
 IDOM_CACHE = "default"
 
-# IDOM works best with a multiprocessing-safe and thread-safe database backend.
+# IDOM requires a multiprocessing-safe and thread-safe database.
 IDOM_DATABASE = "default"
 
 # Maximum seconds between reconnection attempts before giving up.
@@ -11,5 +11,5 @@ IDOM_RECONNECT_MAX = 259200
 # The URL for IDOM to serve the component rendering websocket
 IDOM_WEBSOCKET_URL = "idom/"
 
-# Dotted path to the default postprocessor function, or `None`
+# Dotted path to the default `django_idom.hooks.use_query` postprocessor function, or `None`
 IDOM_DEFAULT_QUERY_POSTPROCESSOR = "example_project.utils.my_postprocessor"
