0b5f747459ddb258a43237ac46d3040e44715e3c
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-03-30 20:12:41 -0700

    increase test db timeout

--- tests/test_app/settings.py
+++ tests/test_app/settings.py
@@ -75,8 +75,12 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        "TEST": {"NAME": os.path.join(BASE_DIR, "db_test.sqlite3")},
-        "OPTIONS": {"timeout": 5},
+        "TEST": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db_test.sqlite3"),
+            "OPTIONS": {"timeout": 20},
+        },
+        "OPTIONS": {"timeout": 20},
     },
 }
 
