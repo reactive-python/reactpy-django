972ceea1347a67014fc0fde77e164cb3ba5cea3f
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-03-31 18:34:15 -0700

    Add rename warning

--- src/django_idom/__init__.py
+++ src/django_idom/__init__.py
@@ -1,8 +1,15 @@
+from logging import getLogger
+
 from django_idom import components, decorators, hooks, types, utils
 from django_idom.websocket.paths import IDOM_WEBSOCKET_PATH
 
 
-__version__ = "3.0.0"
+logger = getLogger(__name__)
+
+print("WARNING: django-idom has been renamed to reactpy-django.")
+logger.warning("django-idom has been renamed to reactpy-django.")
+
+__version__ = "3.0.1"
 __all__ = [
     "IDOM_WEBSOCKET_PATH",
     "hooks",
