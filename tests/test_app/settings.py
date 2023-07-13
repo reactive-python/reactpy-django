8e8cb72885749d7044ef57479da15487b30935ce
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-07-12 21:10:17 -0700

    add whitenoise

--- tests/test_app/settings.py
+++ tests/test_app/settings.py
@@ -13,7 +13,6 @@ import os
 import sys
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SRC_DIR = BASE_DIR.parent / "src"
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
