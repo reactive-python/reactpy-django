4f934a22af34147b87761f5e9a84d3b1bcaf5155
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-08-13 17:13:22 -0700

    slimmer comment

--- src/reactpy_django/__init__.py
+++ src/reactpy_django/__init__.py
@@ -15,9 +15,9 @@ __all__ = [
     "utils",
     "checks",
 ]
-# Built-in asyncio event loops can create `assert f is self._write_fut` exceptions
-# while we are using our backhaul thread with Uvicorn, so we use this patch to fix this.
-# This also resolves jittery rendering behaviors within Daphne. Can be demonstrated
-# using our "Renders Per Second" test page.
+
+# Fixes bugs with REACTPY_BACKHAUL_THREAD + built-in asyncio event loops.
+# Previously, Uvicorn could generate `assert f is self._write_fut` exceptions, and Daphne
+# had jittery rendering behaviors. Demonstrated using our "Renders Per Second" test page.
 with contextlib.suppress(ValueError):
     nest_asyncio.apply()
