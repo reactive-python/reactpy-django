5fc7c84b176228d435c36b274fe2153e2d018904
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-01-08 23:19:13 -0800

    assert if build failed

--- tests/test_app/__init__.py
+++ tests/test_app/__init__.py
@@ -4,5 +4,5 @@ from nodejs import npm
 
 # Make sure the JS is always re-built before running the tests
 js_dir = Path(__file__).parent.parent.parent / "src" / "js"
-npm.call(["install"], cwd=str(js_dir))
-npm.call(["run", "build"], cwd=str(js_dir))
+assert npm.call(["install"], cwd=str(js_dir)) == 0
+assert npm.call(["run", "build"], cwd=str(js_dir)) == 0
