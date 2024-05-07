174a1823df6d369bece6008220de303533a866f0
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-05-07 15:25:50 -0700

    fix mypy lint error

--- src/reactpy_django/http/urls.py
+++ src/reactpy_django/http/urls.py
@@ -7,12 +7,12 @@ app_name = "reactpy"
 urlpatterns = [
     path(
         "web_module/<path:file>",
-        views.web_modules_file,  # type: ignore[arg-type]
+        views.web_modules_file,
         name="web_modules",
     ),
     path(
         "iframe/<str:dotted_path>",
-        views.view_to_iframe,  # type: ignore[arg-type]
+        views.view_to_iframe,
         name="view_to_iframe",
     ),
 ]
