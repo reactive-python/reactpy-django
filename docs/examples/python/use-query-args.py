22fe98f9eabe015baf5f2be5ba85ecf3eb284a94
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-06-18 21:00:33 -0700

    small adjustment for use query args example

--- docs/examples/python/use-query-args.py
+++ docs/examples/python/use-query-args.py
@@ -7,6 +7,6 @@ def example_query(value: int, other_value: bool = False): ...
 
 @component
 def my_component():
-    query = use_query(example_query, kwargs={"value": 123, "other_value": True})
+    query = use_query(example_query, {"value": 123, "other_value": True})
 
     return str(query.data)
