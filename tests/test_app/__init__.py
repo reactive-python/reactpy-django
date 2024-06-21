be6a8d5790dde99302dcbf7b1c24ef5ecaf9535f
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-06-20 22:02:42 -0700

    workflow for pyscript dist

--- tests/test_app/__init__.py
+++ tests/test_app/__init__.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 from nodejs import npm
@@ -6,3 +7,18 @@ from nodejs import npm
 js_dir = Path(__file__).parent.parent.parent / "src" / "js"
 assert npm.call(["install"], cwd=str(js_dir)) == 0
 assert npm.call(["run", "build"], cwd=str(js_dir)) == 0
+
+# Make sure the current PyScript distribution is available
+pyscript_dist = js_dir / "node_modules" / "@pyscript" / "core" / "dist"
+pyscript_static_dir = (
+    Path(__file__).parent.parent.parent
+    / "src"
+    / "reactpy_django"
+    / "static"
+    / "reactpy_django"
+    / "pyscript"
+)
+if not pyscript_static_dir.exists():
+    pyscript_static_dir.mkdir()
+for file in pyscript_dist.iterdir():
+    shutil.copy(file, pyscript_static_dir / file.name)
