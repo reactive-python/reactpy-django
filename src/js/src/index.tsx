44cb785bfe412b612c8e30971cfb34868eaaa30e
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-10-21 23:05:49 -0700

    Bump javascript dependencies

--- src/js/src/index.tsx
+++ src/js/src/index.tsx
@@ -1,81 +1,83 @@
 import { ReactPyDjangoClient } from "./client";
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 import { Layout } from "@reactpy/client/src/components";
 
 export function mountComponent(
-  mountElement: HTMLElement,
-  host: string,
-  urlPrefix: string,
-  componentPath: string,
-  resolvedJsModulesPath: string,
-  reconnectStartInterval: number,
-  reconnectMaxInterval: number,
-  reconnectMaxRetries: number,
-  reconnectBackoffMultiplier: number,
+    mountElement: HTMLElement,
+    host: string,
+    urlPrefix: string,
+    componentPath: string,
+    resolvedJsModulesPath: string,
+    reconnectStartInterval: number,
+    reconnectMaxInterval: number,
+    reconnectMaxRetries: number,
+    reconnectBackoffMultiplier: number,
 ) {
-  // Protocols
-  let httpProtocol = window.location.protocol;
-  let wsProtocol = `ws${httpProtocol === "https:" ? "s" : ""}:`;
+    // Protocols
+    let httpProtocol = window.location.protocol;
+    let wsProtocol = `ws${httpProtocol === "https:" ? "s" : ""}:`;
 
-  // WebSocket route (for Python components)
-  let wsOrigin: string;
-  if (host) {
-    wsOrigin = `${wsProtocol}//${host}`;
-  } else {
-    wsOrigin = `${wsProtocol}//${window.location.host}`;
-  }
-
-  // HTTP route (for JavaScript modules)
-  let httpOrigin: string;
-  let jsModulesPath: string;
-  if (host) {
-    httpOrigin = `${httpProtocol}//${host}`;
-    jsModulesPath = `${urlPrefix}/web_module`;
-  } else {
-    httpOrigin = `${httpProtocol}//${window.location.host}`;
-    if (resolvedJsModulesPath) {
-      jsModulesPath = resolvedJsModulesPath;
+    // WebSocket route (for Python components)
+    let wsOrigin: string;
+    if (host) {
+        wsOrigin = `${wsProtocol}//${host}`;
     } else {
-      jsModulesPath = `${urlPrefix}/web_module`;
+        wsOrigin = `${wsProtocol}//${window.location.host}`;
     }
-  }
 
-  // Embed the initial HTTP path into the WebSocket URL
-  let componentUrl = new URL(`${wsOrigin}/${urlPrefix}/${componentPath}`);
-  componentUrl.searchParams.append("http_pathname", window.location.pathname);
-  if (window.location.search) {
-    componentUrl.searchParams.append("http_search", window.location.search);
-  }
+    // HTTP route (for JavaScript modules)
+    let httpOrigin: string;
+    let jsModulesPath: string;
+    if (host) {
+        httpOrigin = `${httpProtocol}//${host}`;
+        jsModulesPath = `${urlPrefix}/web_module`;
+    } else {
+        httpOrigin = `${httpProtocol}//${window.location.host}`;
+        if (resolvedJsModulesPath) {
+            jsModulesPath = resolvedJsModulesPath;
+        } else {
+            jsModulesPath = `${urlPrefix}/web_module`;
+        }
+    }
 
-  // Configure a new ReactPy client
-  const client = new ReactPyDjangoClient({
-    urls: {
-      componentUrl: componentUrl,
-      query: document.location.search,
-      jsModules: `${httpOrigin}/${jsModulesPath}`,
-    },
-    reconnectOptions: {
-      startInterval: reconnectStartInterval,
-      maxInterval: reconnectMaxInterval,
-      backoffMultiplier: reconnectBackoffMultiplier,
-      maxRetries: reconnectMaxRetries,
-    },
-    mountElement: mountElement,
-    prerenderElement: document.getElementById(mountElement.id + "-prerender"),
-    offlineElement: document.getElementById(mountElement.id + "-offline"),
-  });
+    // Embed the initial HTTP path into the WebSocket URL
+    let componentUrl = new URL(`${wsOrigin}/${urlPrefix}/${componentPath}`);
+    componentUrl.searchParams.append("http_pathname", window.location.pathname);
+    if (window.location.search) {
+        componentUrl.searchParams.append("http_search", window.location.search);
+    }
 
-  // Replace the prerender element with the real element on the first layout update
-  if (client.prerenderElement) {
-    client.onMessage("layout-update", ({ path, model }) => {
-      if (client.prerenderElement) {
-        client.prerenderElement.replaceWith(client.mountElement);
-        client.prerenderElement = null;
-      }
+    // Configure a new ReactPy client
+    const client = new ReactPyDjangoClient({
+        urls: {
+            componentUrl: componentUrl,
+            query: document.location.search,
+            jsModules: `${httpOrigin}/${jsModulesPath}`,
+        },
+        reconnectOptions: {
+            startInterval: reconnectStartInterval,
+            maxInterval: reconnectMaxInterval,
+            backoffMultiplier: reconnectBackoffMultiplier,
+            maxRetries: reconnectMaxRetries,
+        },
+        mountElement: mountElement,
+        prerenderElement: document.getElementById(
+            mountElement.id + "-prerender",
+        ),
+        offlineElement: document.getElementById(mountElement.id + "-offline"),
     });
-  }
 
-  // Start rendering the component
-  render(<Layout client={client} />, client.mountElement);
+    // Replace the prerender element with the real element on the first layout update
+    if (client.prerenderElement) {
+        client.onMessage("layout-update", ({ path, model }) => {
+            if (client.prerenderElement) {
+                client.prerenderElement.replaceWith(client.mountElement);
+                client.prerenderElement = null;
+            }
+        });
+    }
+
+    // Start rendering the component
+    ReactDOM.render(<Layout client={client} />, client.mountElement);
 }
