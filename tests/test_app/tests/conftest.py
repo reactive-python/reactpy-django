437f87f5a1dcd1f57810111a4308e78ea14bf4ca
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-11-27 02:39:51 -0800

    Auto install playwright in conftest

--- tests/test_app/tests/conftest.py
+++ tests/test_app/tests/conftest.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+import subprocess
 from pathlib import Path
 
 import pytest
@@ -11,3 +12,8 @@ os.chdir(Path(__file__).parent.parent.parent)
 @pytest.fixture(autouse=True)
 def enable_db_access_for_all_tests(db):
     pass
+
+
+@pytest.fixture(autouse=True, scope="session")
+def install_playwright():
+    subprocess.run(["playwright", "install", "chromium"], check=True)
