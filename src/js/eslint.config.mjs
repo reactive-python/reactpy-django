1597441add3d31925ad78aee18cde8d3ad552dc9
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-11-28 15:25:38 -0800

    new format for eslint config

---
+++ src/js/eslint.config.mjs
@@ -0,0 +1,43 @@
+import react from "eslint-plugin-react";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends("eslint:recommended", "plugin:react/recommended"),
+  {
+    plugins: {
+      react,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+
+    settings: {
+      react: {
+        version: "18.2.0",
+      },
+    },
+
+    rules: {
+      "react/prop-types": "off",
+    },
+  },
+];
