35b4d550a751cd21f1d576b171f254df7819fea7
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-12-01 21:03:06 -0800

    utilize underscore instead of dash for docs/examples/

--- docs/examples/python/example/components.py
+++ docs/examples/python/example/components.py
@@ -1,1 +1,6 @@
+"""This module exists only to satisfy type checkers.
+
+Do not use the files in this module as examples within the docs."""
+
+
 def child_component(): ...
--- docs/examples/python/example/views.py
+++ docs/examples/python/example/views.py
@@ -1,3 +1,7 @@
+"""This module exists only to satisfy type checkers.
+
+Do not use the files in this module as examples within the docs."""
+
 from python.hello_world_cbv import HelloWorld
 from python.hello_world_fbv import hello_world
 
