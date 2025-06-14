f320a4400e6164165717dc9d4b6478394aa8fa6b
Archmonger <16909269+Archmonger@users.noreply.github.com>
2025-06-14 04:48:24 -0700

    add stack to error msg

--- tests/test_app/tests/utils.py
+++ tests/test_app/tests/utils.py
@@ -118,7 +118,7 @@ class PlaywrightTestCase(ChannelsLiveServerTestCase):
         cls.page = cls.browser.new_page()
         cls.page.set_default_timeout(10000)
         cls.page.on("console", lambda msg: print(f"CLIENT {msg.type.upper()}: {msg.text}"))
-        cls.page.on("pageerror", lambda err: print(f"CLIENT EXCEPTION: {err.name}: {err.message}"))
+        cls.page.on("pageerror", lambda err: print(f"CLIENT EXCEPTION: {err.name}: {err.message}\n{err.stack}"))
 
     @classmethod
     def shutdown_playwright_client(cls):
