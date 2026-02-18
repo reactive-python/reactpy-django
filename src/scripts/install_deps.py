e221ded9e395dddb50a8d013ac1df21d05b8b020
Archmonger <16909269+Archmonger@users.noreply.github.com>
2026-02-17 19:52:54 -0800

    Add new dev helpful scripts

---
+++ src/scripts/install_deps.py
@@ -0,0 +1,37 @@
+"""
+Development/debug script to parse pyproject.toml to find dependecies then install them in the local
+environment via `uv pip install -U <pkg_names>`
+"""
+
+import subprocess
+from pathlib import Path
+
+import toml
+
+DEPENDENCIES = set()
+
+
+def find_deps(data):
+    """Recurse through all categories and find any list with `dependencies` in the name, then combine
+    all dependencies into a single list"""
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if "dependencies" in key and isinstance(value, list) and value and isinstance(value[0], str):
+                DEPENDENCIES.update(value)
+            else:
+                find_deps(value)
+    elif isinstance(data, list):
+        for item in data:
+            find_deps(item)
+
+
+def install_deps():
+    pyproject_path = Path(__file__).parent.parent.parent / "pyproject.toml"
+    pyproject_data = toml.load(pyproject_path)
+    find_deps(pyproject_data)
+    DEPENDENCIES.remove("ruff")  # ruff only exists in dev dependencies for CI purposes.
+    subprocess.run(["uv", "pip", "install", "-U", *DEPENDENCIES], check=False)  # noqa: S607
+
+
+if __name__ == "__main__":
+    install_deps()
